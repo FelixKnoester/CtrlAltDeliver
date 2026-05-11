dc8ef31450e6b6890fa38e3d8c1aebd209d9e12a
FelixKnoester <825119@vo.saro.org>
2026-05-11 16:36:36 +0200

    Add files via upload

---
+++ Script_Main-main.py
@@ -0,0 +1,12 @@
+# Imports go at the top
+from microbit import *
+#from X import *
+#from X import *
+#from X import *
+
+
+while True:
+    pin14.write_analog(75) #startpositie
+    sleep(1000)
+    
+    pin14.write_analog(125) #eindpositie
